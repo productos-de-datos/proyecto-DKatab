41f8a931dec69905dc2a7c335056253fd56c0380
DKatab <101578930+DKatab@users.noreply.github.com>
2022-07-01 15:37:12 -0500

    Avance3

--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -1,33 +1,64 @@
-from pandas import to_pickle
-
-
 def train_daily_model():
     """Entrena el modelo de pronóstico de precios diarios.
-
     Con las features entrene el modelo de proóstico de precios diarios y
     salvelo en models/precios-diarios.pkl
-
-
     """
-    from cgi import test
-    import pandas as pd
-    from sklearn import linear_model
+
     import pickle
-   
+    from sklearn.neural_network import MLPRegressor
+    import pandas as pd
+    from sklearn.preprocessing import MinMaxScaler
+    import matplotlib.pyplot as plt
+    import numpy as np
+    
+    precios_diarios = pd.read_csv('../../data_lake/business/precios-diarios.csv', sep = ',')
+
+    scaler = MinMaxScaler()
+
+    # escala la serie
+    data_scaled = scaler.fit_transform(np.array(precios_diarios['Precio']).reshape(-1, 1))
+
+    # z es un array de listas como efecto
+    # del escalamiento
+    data_scaled = [u[0] for u in data_scaled]
+
+    data_d1 = [precios_diarios['Precio'][t] - precios_diarios['Precio'][t - 1] for t in range(1, len(precios_diarios['Precio']))]
+
+    data_d1d12 = [data_d1[t] - data_d1[t - 12] for t in range(12, len(data_d1))]
+
+    data_d1d12_scaled = scaler.fit_transform(np.array(data_d1d12).reshape(-1, 1))
+    data_d1d12_scaled = [u[0] for u in data_d1d12_scaled]
 
 
+    P = 13
 
-    # Lea el archivo `insurance.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('../../data_lake/business/features/precios-diarios.csv')
-    df = df.dropna()
+    X = []
+    for t in range(P - 1, len(data_d1d12_scaled) - 1):
+        X.append([data_d1d12_scaled[t - n] for n in range(P)])
 
-    # Asigne la columna `charges` a la variable `y`.
-    y = df['Precio']
-    X = df[['Diferencia_precio','Dia_semana','Dia_mes','Precio_ayer']]
+    d = data_d1d12_scaled[P:]
 
-    regr = linear_model.LinearRegression()
-    regr.fit(X, y)
-    pickle.dump(regr, open('../models/precios-diarios.pkl','wb'))
+    H = 4  # Se escoge arbitrariamente
+
+    np.random.seed(123456)
+
+    mlp = MLPRegressor(
+        hidden_layer_sizes=(H,),
+        activation="logistic",
+        learning_rate="adaptive",
+        momentum=0.0,
+        learning_rate_init=0.002,
+        max_iter=100000,
+    )
+
+    # Entrenamiento
+    mlp.fit(X[0:9383], data_d1d12_scaled[0:9383]) 
+
+    filename = '../models/precios-diarios.pkl'
+    pickle.dump(mlp, open(filename, 'wb'))
+
+
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
