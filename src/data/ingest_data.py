aa5a52ff08ab681650ab0763b92f909e73c94308
DKatab <101578930+DKatab@users.noreply.github.com>
2022-07-01 16:50:05 -0500

    avance 8

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -17,25 +17,24 @@ def ingest_data():
     # realizaremos la ingesta de datos. Esta información se depositará en la carpta landing que creamos en el 
     # anterior.
     
-    import github
-    import requests
-    
-    g = github.Github()
-    repo = g.get_repo('jdvelasq/datalabs')
-    contents = repo.get_contents('datasets/precio_bolsa_nacional/xls')
+     
+    import requests as req
 
-    for contentFile in contents:
-        url_descarga = contentFile.download_url
-        nombre_archivo = url_descarga.rsplit('/', 1)[1]
-        with open('data_lake/landing/' + nombre_archivo, 'wb') as f:
-            f.write(requests.get(url_descarga).content)
+    for year in range(1995, 2022):
+        if year in range(2016, 2018):
+            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(year)
+            file = req.get(url, allow_redirects=True)
+            open('data_lake/landing/{}.xls'.format(year), 'wb').write(file.content)
+        else:
+            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(year)
+            file = req.get(url, allow_redirects=True)
+            open('data_lake/landing/{}.xlsx'.format(year), 'wb').write(file.content)
 
     #raise NotImplementedError("Implementar esta función")
-
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
-    
+
     ingest_data()
