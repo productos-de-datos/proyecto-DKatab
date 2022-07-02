4e22d4e8bf5eda1e2e7913c6f89c206a1b8c21c5
DKatab <101578930+DKatab@users.noreply.github.com>
2022-07-01 23:09:25 -0500

    avance 34

--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -1,82 +1,70 @@
-"""Entrena el modelo de pronóstico de precios diarios.
+def train_daily_model():
+    """Entrena el modelo de pronóstico de precios diarios.
     Con las features entrene el modelo de proóstico de precios diarios y
     salvelo en models/precios-diarios.pkl
     """
-    
-
-def load_data():
-
-    import pandas as pd
-
-    in_path = 'data_lake/business/features/precios_diarios.csv'
-    data = pd.read_csv(in_path, sep=",")
-
-    return data
-
-
-def data_preparation(data):
-    import pandas as pd
-    df = data.copy()
-    df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
-    df['year'], df['month'], df['day'] = df['fecha'].dt.year, df['fecha'].dt.month, df['fecha'].dt.day
-
-    y = df["precio"]
-    x = df.copy()
-    x.pop("precio")
-    x.pop("fecha")
-    return x, y
-
-
-def make_train_test_split(x, y):
-
-    from sklearn.model_selection import train_test_split
-
-    (x_train, x_test, y_train, y_test) = train_test_split(
-        x,
-        y,
-        test_size=0.25,
-        random_state=12345,
-    )
-    return x_train, x_test, y_train, y_test
-
-
-def trein_model(x_train, x_test):
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.ensemble import RandomForestRegressor
-
-    # Se Define el algoritmo a utilizar
-    scaler = StandardScaler()
-    scaler.fit(x_train)
-    x_train = scaler.transform(x_train)
-    x_test = scaler.transform(x_test)
-
-    # Se establece el Modelo
-    model_RF = RandomForestRegressor(n_jobs=-1)
-
-    return model_RF
-
-
-def save_model(model_RF):
 
     import pickle
+    from sklearn.neural_network import MLPRegressor
+    import pandas as pd
+    from sklearn.preprocessing import MinMaxScaler
+    import matplotlib.pyplot as plt
+    import numpy as np
+    
+    precios_diarios = pd.read_csv('data_lake/business/precios-diarios.csv', sep = ',')
 
-    with open("src/models/precios-diarios.pkl", "wb") as file:
-        pickle.dump(model_RF, file,  pickle.HIGHEST_PROTOCOL)
+    scaler = MinMaxScaler()
+
+    # escala la serie
+    data_scaled = scaler.fit_transform(np.array(precios_diarios['precio']).reshape(-1, 1))
+
+    # z es un array de listas como efecto
+    # del escalamiento
+    data_scaled = [u[0] for u in data_scaled]
+
+    data_d1 = [precios_diarios['precio'][t] - precios_diarios['precio'][t - 1] for t in range(1, len(precios_diarios['precio']))]
+
+    data_d1d12 = [data_d1[t] - data_d1[t - 12] for t in range(12, len(data_d1))]
+
+    data_d1d12_scaled = scaler.fit_transform(np.array(data_d1d12).reshape(-1, 1))
+    data_d1d12_scaled = [u[0] for u in data_d1d12_scaled]
 
 
-def train_daily_model():
-    try:
-        data = load_data()
-        x, y = data_preparation(data)
-        x_train, x_test, y_train, y_test = make_train_test_split(x, y)
-        model_RF = trein_model(x_train, x_test)
-        save_model(model_RF)
-    except:
-        raise NotImplementedError("Implementar esta función")
+    P = 13
+
+    X = []
+    for t in range(P - 1, len(data_d1d12_scaled) - 1):
+        X.append([data_d1d12_scaled[t - n] for n in range(P)])
+
+    d = data_d1d12_scaled[P:]
+
+    H = 4  # Se escoge arbitrariamente
+
+    np.random.seed(123456)
+
+    mlp = MLPRegressor(
+        hidden_layer_sizes=(H,),
+        activation="logistic",
+        learning_rate="adaptive",
+        momentum=0.0,
+        learning_rate_init=0.002,
+        max_iter=100000,
+    )
+
+    # Entrenamiento
+    mlp.fit(X[0:9383], data_d1d12_scaled[0:9383]) 
+
+    filename = 'src/models/precios-diarios.pkl'
+    pickle.dump(mlp, open(filename, 'wb'))
+
+
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+
     train_daily_model()
+
